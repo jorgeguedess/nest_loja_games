42afcadbc32f9ee0c036c9ffc350db691da13f7e
jorgeguedess <jorge_guedes00@hotmail.com>
2024-04-25 13:42:13 -0300

    feat: added secure login authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { Bcrypt } from './bcrypt/bcrypt';
+import { UsuarioModule } from 'src/usuario/usuario.module';
+import { jwtConstants } from './constants/constants';
+import { AuthService } from './services/auth.service';
+import { LocalStrategy } from './strategy/local.strategy';
+import { AuthController } from './controllers/auth.controller';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  imports: [],
-  providers: [Bcrypt],
-  controllers: [],
+  imports: [
+    UsuarioModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [Bcrypt, AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
   exports: [Bcrypt],
 })
 export class AuthModule {}
